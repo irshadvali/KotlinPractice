3b184b1b18f92d46f1c8864edcc2380d050388e8
irshadvali <irshad@xelpmoc.in>
2017-09-22 22:30:01 +0530

    function

---
+++ app/src/main/java/com/irshad/kotlinpractice/practice/BasicFunction.kt
@@ -0,0 +1,16 @@
+package com.irshad.kotlinpractice.practice
+
+/**
+ * Created by irshadvali on 22/09/17.
+ */
+
+fun main(args: Array<String>){
+    var myname: String
+    myname = "Irshad Vali"
+    display(myname)
+}
+
+fun display(name: String){
+    println(name)
+
+}
